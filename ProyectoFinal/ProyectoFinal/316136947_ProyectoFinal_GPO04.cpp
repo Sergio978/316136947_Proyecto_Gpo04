64734d640b5d8de4913811108d66d9b334c99932
Sergio Guzmán <sergiofranc9@outlook.com>
2022-05-05 21:47:07 -0500

    Adding complex animation, correction of ligth and sound

--- ProyectoFinal/ProyectoFinal/316136947_ProyectoFinal_GPO04.cpp
+++ ProyectoFinal/ProyectoFinal/316136947_ProyectoFinal_GPO04.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include<windows.h>
 #include <cmath>
 
-// GLEW
+// GLEW is a library that provides methods that can be implemented at runtime
 #include <GL/glew.h>
 
-// GLFW
+// GLFW is a library dedicated to the management of windows and keyboard events
 #include <GLFW/glfw3.h>
 
-// Other Libs
+// Other Libs for image loading
 #include "stb_image.h"
 
 // GLM Mathematics
@@ -15,9 +16,8 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-//Load Models
+//Load Models and for uploading textures into OpenGL
 #include "SOIL2/SOIL2.h"
-
 
 // Other includes
 #include "Shader.h"
@@ -25,25 +25,36 @@
 #include "Model.h"
 
 // Function prototypes
-void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
-void MouseCallback(GLFWwindow* window, double xPos, double yPos);
-void DoMovement();
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode); //This function is called when a especified key via GLFW is pressed
+void MouseCallback(GLFWwindow* window, double xPos, double yPos);// This function is for the camera configuration using mouse as input 
+void DoMovement(); //The function indicates what will be performed by the keys that are pressed and under which strict event
 
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
-// Camera
-Camera camera(glm::vec3(10.0f, 10.0f, 35.0f));
+// Camera:
+Camera camera(glm::vec3(10.0f, 10.0f, 35.0f)); //An instance of the camera class is declared and initialized
+/* lastXand lastY, 
+allow you to store the last mouse positions in the app, which we initialize to be in the center 
+of the screen(the screen size is 800 by 600) initially */
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
-bool keys[1024];
-bool firstMouse = true;
+
+bool keys[1024]; //An array is defined, this stores the key set via GLFW 
+bool firstMouse = true; //Defining a global bool variable to check if this is the first time we receive mouse input
+
 // Light attributes
 glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
+
 //Animation
+/*
+	Variables with which it is allowed to start and perform a certain animation
+*/
 bool active;
 float rot = 0.0f;
+float rotP = 0.0f;
 bool open = false;
 bool close = true;
 bool anim = false;
@@ -54,17 +65,29 @@ bool close2 = true;
 bool anim_tres = false;
 float giro = 0.0f;
 
+bool anim_P = false;
+float rise = 9.1;
+float scale= 1.5;
+float PosY[380] = { 9.14105, 9.18211, 9.22316, 9.26421, 9.30526, 9.34632, 9.38737, 9.42842, 9.46948, 9.51053, 9.55158, 9.59263, 9.63369, 9.67474, 9.71579, 9.75685, 9.7979, 9.83895, 9.88, 9.92106, 9.96211, 10.0032, 10.0442, 10.0853, 10.1263, 10.1674, 10.2084, 10.2495, 10.2905, 10.3316, 10.3726, 10.4137, 10.4547, 10.4958, 10.5368, 10.5779, 10.619, 10.66, 10.7011, 10.7421, 10.7832, 10.8242, 10.8653, 10.9063, 10.9474, 10.9884, 11.0295, 11.0705, 11.1116, 11.1526, 11.1937, 11.2347, 11.2758, 11.3169, 11.3579, 11.399, 11.44, 11.4811, 11.5221, 11.5632, 11.6042, 11.6453, 11.6863, 11.7274, 11.7684, 11.8095, 11.8505, 11.8916, 11.9326, 11.9737, 12.0148, 12.0558, 12.0969, 12.1379, 12.179, 12.22, 12.2611, 12.3021, 12.3432, 12.3842, 12.4253, 12.4663, 12.5074, 12.5484, 12.5895, 12.6305, 12.6716, 12.7126, 12.7537, 12.7948, 12.8358, 12.8769, 12.9179, 12.959, 13, 13.0411, 13.0821, 13.1232, 13.1642, 13.2053, 13.2463, 13.2874, 13.3284, 13.3695, 13.4105, 13.4516, 13.4927, 13.5337, 13.5748, 13.6158, 13.6569, 13.6979, 13.739, 13.78, 13.8211, 13.8621, 13.9032, 13.9442, 13.9853, 14.0263, 14.0674, 14.1084, 14.1495, 14.1905, 14.2316, 14.2727, 14.3137, 14.3548, 14.3958, 14.4369, 14.4779, 14.519, 14.56, 14.6011, 14.6421, 14.6832, 14.7242, 14.7653, 14.8063, 14.8474, 14.8884, 14.9295, 14.9706, 15.0116, 15.0527, 15.0937, 15.1348, 15.1758, 15.2169, 15.2579, 15.299, 15.34, 15.3811, 15.4221, 15.4632, 15.5042, 15.5453, 15.5863, 15.6274, 15.6685, 15.7095, 15.7506, 15.7916, 15.8327, 15.8737, 15.9148, 15.9558, 15.9969, 16.0379, 16.079, 16.12, 16.1611, 16.2021, 16.2432, 16.2842, 16.3253, 16.3663, 16.4074, 16.4484, 16.4895, 16.5305, 16.5716, 16.6127, 16.6537, 16.6948, 16.7358, 16.7769, 16.8179, 16.859, 16.9, 16.8579, 16.8158, 16.7737, 16.7316, 16.6895, 16.6474, 16.6053, 16.5632, 16.5211, 16.479, 16.4369, 16.3948, 16.3527, 16.3105, 16.2684, 16.2263, 16.1842, 16.1421, 16.1, 16.0579, 16.0158, 15.9737, 15.9316, 15.8895, 15.8474, 15.8053, 15.7632, 15.7211, 15.679, 15.6369, 15.5948, 15.5527, 15.5105, 15.4684, 15.4263, 15.3842, 15.3421, 15.3, 15.2579, 15.2158, 15.1737, 15.1316, 15.0895, 15.0474, 15.0053, 14.9632, 14.9211, 14.879, 14.8369, 14.7948, 14.7526, 14.7105, 14.6684, 14.6263, 14.5842, 14.5421, 14.5, 14.4579, 14.4158, 14.3737, 14.3316, 14.2895, 14.2474, 14.2053, 14.1632, 14.1211, 14.079, 14.0368, 13.9947, 13.9526, 13.9105, 13.8684, 13.8263, 13.7842, 13.7421, 13.7, 13.6579, 13.6158, 13.5737, 13.5316, 13.4895, 13.4474, 13.4053, 13.3632, 13.3211, 13.2789, 13.2368, 13.1947, 13.1526, 13.1105, 13.0684, 13.0263, 12.9842, 12.9421, 12.9, 12.8579, 12.8158, 12.7737, 12.7316, 12.6895, 12.6474, 12.6053, 12.5632, 12.521, 12.4789, 12.4368, 12.3947, 12.3526, 12.3105, 12.2684, 12.2263, 12.1842, 12.1421, 12.1, 12.0579, 12.0158, 11.9737, 11.9316, 11.8895, 11.8474, 11.8052, 11.7631, 11.721, 11.6789, 11.6368, 11.5947, 11.5526, 11.5105, 11.4684, 11.4263, 11.3842, 11.3421, 11.3, 11.2579, 11.2158, 11.1737, 11.1316, 11.0895, 11.0473, 11.0052, 10.9631, 10.921, 10.8789, 10.8368, 10.7947, 10.7526, 10.7105, 10.6684, 10.6263, 10.5842, 10.5421, 10.5, 10.4579, 10.4158, 10.3737, 10.3315, 10.2894, 10.2473, 10.2052, 10.1631, 10.121, 10.0789, 10.0368, 9.9947, 9.9526, 9.91049, 9.86839, 9.82628, 9.78418, 9.74207, 9.69996, 9.65786, 9.61575, 9.57365, 9.53154, 9.48944, 9.44733, 9.40522, 9.36312, 9.32101, 9.27891, 9.2368, 9.1947, 9.15259, 9.11049, 9.06838, 9.02627, 8.98417, 8.94206, 8.89996 };
 
+float Esc[380] = { 1.30105, 1.30211, 1.30316, 1.30421, 1.30526, 1.30632, 1.30737, 1.30842, 1.30947, 1.31053, 1.31158, 1.31263, 1.31368, 1.31474, 1.31579, 1.31684, 1.31789, 1.31895, 1.32, 1.32105, 1.3221, 1.32316, 1.32421, 1.32526, 1.32632, 1.32737, 1.32842, 1.32947, 1.33053, 1.33158, 1.33263, 1.33368, 1.33474, 1.33579, 1.33684, 1.33789, 1.33895, 1.34, 1.34105, 1.3421, 1.34316, 1.34421, 1.34526, 1.34632, 1.34737, 1.34842, 1.34947, 1.35053, 1.35158, 1.35263, 1.35368, 1.35474, 1.35579, 1.35684, 1.35789, 1.35895, 1.36, 1.36105, 1.3621, 1.36316, 1.36421, 1.36526, 1.36631, 1.36737, 1.36842, 1.36947, 1.37053, 1.37158, 1.37263, 1.37368, 1.37474, 1.37579, 1.37684, 1.37789, 1.37895, 1.38, 1.38105, 1.3821, 1.38316, 1.38421, 1.38526, 1.38631, 1.38737, 1.38842, 1.38947, 1.39053, 1.39158, 1.39263, 1.39368, 1.39474, 1.39579, 1.39684, 1.39789, 1.39895, 1.4, 1.40105, 1.4021, 1.40316, 1.40421, 1.40526, 1.40631, 1.40737, 1.40842, 1.40947, 1.41052, 1.41158, 1.41263, 1.41368, 1.41474, 1.41579, 1.41684, 1.41789, 1.41895, 1.42, 1.42105, 1.4221, 1.42316, 1.42421, 1.42526, 1.42631, 1.42737, 1.42842, 1.42947, 1.43052, 1.43158, 1.43263, 1.43368, 1.43474, 1.43579, 1.43684, 1.43789, 1.43895, 1.44, 1.44105, 1.4421, 1.44316, 1.44421, 1.44526, 1.44631, 1.44737, 1.44842, 1.44947, 1.45052, 1.45158, 1.45263, 1.45368, 1.45473, 1.45579, 1.45684, 1.45789, 1.45895, 1.46, 1.46105, 1.4621, 1.46316, 1.46421, 1.46526, 1.46631, 1.46737, 1.46842, 1.46947, 1.47052, 1.47158, 1.47263, 1.47368, 1.47473, 1.47579, 1.47684, 1.47789, 1.47895, 1.48, 1.48105, 1.4821, 1.48316, 1.48421, 1.48526, 1.48631, 1.48737, 1.48842, 1.48947, 1.49052, 1.49158, 1.49263, 1.49368, 1.49473, 1.49579, 1.49684, 1.49789, 1.49894, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5, 1.5 };
+
+int i=0;
 // Positions of the point lights
 glm::vec3 pointLightPositions[] = {
-	glm::vec3(-6.9f, 6.75f, 6.4f),
-	glm::vec3(-6.9f, 6.75f, -4.2f),
-	glm::vec3(4.5f,15.6f,-2.5f),
-	glm::vec3(10.8f, 11.75f, 3.2f),
-	glm::vec3(-5.05f, 5.0f, -8.48f)
+	glm::vec3(-6.9f, 6.75f, 6.4f), //Located in the first ceiling lamp inside the living room of the house
+	glm::vec3(-6.9f, 6.75f, -4.2f), //Located in the second ceiling lamp inside the living room of the house
+	glm::vec3(4.5f,15.6f,-2.5f), //Located in the ceiling lamp inside tommy's room
+	glm::vec3(10.8f, 11.75f, 3.2f), //Located in the light bulb of the lamp object at tommy's room
+	glm::vec3(-5.05f, 5.0f, -8.48f) //Located in the light bulb of the lamp object at the living room of the house
 };
 
+/*
+Vertices data are defined, in this case position and normals; 
+the geometry of the vertices form a cube, it will be used if it's necessary
+*/
 float vertices[] = {
+	//Position             //Normals
 	 -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 		0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
 		0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -108,36 +131,55 @@ float vertices[] = {
 	   -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
 };
 
+/*
+A vector 3 is declared for each light element, 
+so that the ambiental and diffuse property can be controlled,
+allowing a light color to be handled
+*/
 glm::vec3 Light1 = glm::vec3(0);
 glm::vec3 Light2 = glm::vec3(0);
 glm::vec3 Light3 = glm::vec3(0);
 glm::vec3 Light4 = glm::vec3(0);
 glm::vec3 Light5 = glm::vec3(0);
 
-// Deltatime
+/*
+Deltatime:
+Stores the time it took to render the last frame. 
+By using this approach the camera speed will be balanced
+*/
 GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
 GLfloat lastFrame = 0.0f;  	// Time of last frame
 
-int main()
+int main() //Main function
 {
-	// Init GLFW
+	//Initializes the GLFW library
 	glfwInit();
-	// Set all the required options for GLFW
-	/*glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	
+	/*
+	Set all the required options for GLFW
+	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	
+	*/
 
 	// Create a GLFWwindow object that we can use for GLFW's functions
 	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Tommy's House", nullptr, nullptr);
 
+	//In case the window object is not created correctly error message is sent in console
 	if (nullptr == window)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
+		/*
+		If there is a fail, this function will destroy all remaining windows and cursors, 
+		restores any modified gamma ramps and frees any other allocated resources.
+		*/
 		glfwTerminate();
 
-		return EXIT_FAILURE;
+		return EXIT_FAILURE; //Defined value for failure, 1
 	}
 
 	glfwMakeContextCurrent(window);
@@ -297,7 +339,7 @@ int main()
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[2].quadratic"), 0.20f);
 
 		// Point light 4
-		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].position"), pointLightPositions[3].x, pointLightPositions[3].y, pointLightPositions[3].z);
+		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].position"), pointLightPositions[3].x, 13.2f, pointLightPositions[3].z);
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].ambient"), Light4.x, Light4.y, Light4.z);
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].diffuse"), Light4.x, Light4.y, Light4.z);
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[3].specular"), 0.0f, 0.0f, 0.0f);
@@ -311,8 +353,8 @@ int main()
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[4].diffuse"), Light5.x, Light5.y, Light5.z);
 		glUniform3f(glGetUniformLocation(lightingShader.Program, "pointLights[4].specular"), 0.0f, 0.0f, 0.0f);
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[4].constant"), 1.0f);
-		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[4].linear"), 0.7f);
-		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[4].quadratic"), 1.8f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[4].linear"), 0.2f);
+		glUniform1f(glGetUniformLocation(lightingShader.Program, "pointLights[4].quadratic"), 1.2f);
 
 		// Set material properties
 		glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 22.0f); //Brillo del objeto
@@ -398,8 +440,9 @@ int main()
 		glBindVertexArray(0);
 
 		model = glm::mat4(1);
-		model = glm::translate(model, glm::vec3(7.2f, 9.1f, 0.0f));
-		model = glm::scale(model, glm::vec3(1.5f, 1.5f, 1.5f));
+		model = glm::translate(model, glm::vec3(7.2f, rise, 0.0f));
+		model = glm::scale(model, glm::vec3(1.5f, scale, 1.5f));
+		model = glm::rotate(model, glm::radians(rotP), glm::vec3(1.0f, 0.0f, 0.0f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		Pelota.Draw(lightingShader);
 		glBindVertexArray(0);
@@ -660,7 +703,7 @@ int main()
 		C_foco.Draw(lampShader);
 		glBindVertexArray(0);
 
-		//For the bulb lamp in the lamp object living room
+		//For the bulb lamp in the lamp object at the living room
 		lampShader.Use();
 
 		// Get location objects for the matrices on the lamp shader
@@ -784,6 +827,29 @@ void DoMovement()
 		}
 	}
 
+	if (anim_P == true) {
+		if (i < 380) {
+			rise = PosY[i];
+			scale = Esc[i];
+			//printf("R: %f\n Es:%f\n", rise, scale);
+			if (i == 379) {
+				i = 0;
+				anim_P = false;
+				rise = 9.1f;
+				scale = 1.5f;
+			}
+			
+		}
+		i++;
+		if (rotP < 381) {
+			printf("%f\n", rotP);
+			rotP++;
+		}
+		if (rotP == 380) {
+			rotP = 0;
+		}
+	}
+
 }
 
 // Is called whenever a key is pressed/released via GLFW
@@ -819,6 +885,11 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
 	if (keys[GLFW_KEY_3])
 	{
 		anim_tres = true;
+		PlaySound(TEXT("Models/Accesorios/Rugrats.wav"), NULL, SND_ASYNC);
+	}
+
+	if (keys[GLFW_KEY_4]) {
+		anim_P = true;
 	}
 
 	if (keys[GLFW_KEY_6])
